a5d11ddf50b2aa78d218764455e5c76db214c663
Jay Ohms <jay@37signals.com>
2024-09-25 10:30:56 -0400

    Use the live iOS/Android version tags

--- _source/_data/android_release.js
+++ _source/_data/android_release.js
@@ -1,9 +1,6 @@
 const Cache = require("@11ty/eleventy-cache-assets");
 
 module.exports = async function() {
-  // TODO: Update date when Android repo goes public.
-  return { url: "https://github.com/hotwired/hotwire-native-android", tag_name: "BETA", created_at: "2024-01-01:00:00:00" }
-
   let json = await Cache("https://api.github.com/repos/hotwired/hotwire-native-android/releases/latest", {
     duration: "1d",
     type: "json"
